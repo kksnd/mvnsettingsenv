420a1bf1867a666e01250173f723cdaa6f2acdee
koki.senda <koki.senda@rakuten.com>
2021-09-14 18:16:03 +0900

    現在のバージョンのバックアップがなかったら作成する

--- mvnsettingsenv.sh
+++ mvnsettingsenv.sh
@@ -18,11 +18,18 @@ fi
 if [ $# -eq 2 ]; then
     if [ $1 = "global" -a -f $M2DIR/settings.xml.$2 ]; then
         echo $2
+        # backup if not exist
+        ls $M2DIR/current_settings
+        current=$(cat $M2DIR/current_settings)
+        if [ ! -f $M2DIR/settings.xml.$current ]; then
+            echo "backup setting.xml => settings.xml.$current"
+            cp $M2DIR/settings.xml $M2DIR/settings.xml.$current
+        fi
         # update settings.xml
         rm $M2DIR/settings.xml
         cp $M2DIR/settings.xml.$2 $M2DIR/settings.xml
         # update currnet info
         rm $M2DIR/current_settings
-        echo $2 > $M2DIR/current_settings
+        echo -n $2 > $M2DIR/current_settings
     fi
 fi
